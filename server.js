9e23c9d3a709d2e663a5e1184c9ae4811128ae4b
Keith Dahlby <keith@newbo.co>
2020-04-08 15:07:47 -0500

    Add different query pattern example

--- server.js
+++ server.js
@@ -62,9 +62,16 @@ app.get('/weather', weatherHandler);
 function weatherHandler(request, response){
   const weatherData = require('./data/darksky.json');
 
+  // const key = process.env.WEATHER_KEY;
+  // const lat = request.query.latitude;
+  // const lon = request.query.longitude;
+
+  // superagent.get('whatever weather')
+  //   .query({ key, lat, lon })
+  //   .then(...)
+
   const latitude = request.query.latitude;
   const longitude = request.query.longitude;
-
   console.log('/weather', { latitude, longitude });
 
   const weatherResults = [];
@@ -108,5 +115,5 @@ function Location(city, geoData) {
 
 function Weather(weatherData) {
   this.forecast = weatherData.summary;
-  this.time = new Date(weatherData.time * 1000);
+  this.time = new Date(weatherData.time * 1000).toDateString();
 }
